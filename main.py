148bb6fc45fd9a35b3a09436fa7376c6c48d4e7d
Ronda <Rondaw555@yandex.ru>
2026-02-15 19:06:26 +0300

    N1 done with scale

--- main.py
+++ main.py
@@ -1,51 +1,130 @@
 import arcade
-from arcade.gui import UIManager, UIFlatButton, UITextureButton, UILabel, UIInputText, UITextArea, UISlider, UIDropdown, \
-    UIMessageBox  # Это разные виджеты
-from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout  # А это менеджеры компоновки, как в pyQT
+from arcade.gui import UIManager, UIFlatButton, UILabel, UIInputText
+import requests
+from PIL import Image
+from io import BytesIO
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+MAP_SIZE = (600, 450)  # размеры png от яндекса
+MAP_SCALE = 1.5  # увелечение png
+SCREEN_WIDTH = MAP_SIZE[0] * MAP_SCALE + 300
+MAP_BORDER = 25
+SCREEN_HEIGHT = MAP_SIZE[1] * MAP_SCALE + MAP_BORDER * 2
 
 
 class MyGUIWindow(arcade.Window):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Супер GUI Пример!")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Яндекс карты")
         arcade.set_background_color(arcade.color.GRAY)
 
-        # UIManager — сердце GUI
         self.manager = UIManager()
-        self.manager.enable()  # Включить, чтоб виджеты работали
+        self.manager.enable()
+        self.gui_elements = []  # для изменение текста в UIInputText
 
-        # Layout для организации — как полки в шкафу
-        self.anchor_layout = UIAnchorLayout()  # Центрирует виджеты
-        self.box_layout = UIBoxLayout(vertical=True, space_between=10)  # Вертикальный стек
+        self.ll = [37.677751, 55.757718]
+        self.spn = [0.016457, 0.00619]
+        self.Map = None
+        self.update_map()
 
-        # Добавим все виджеты в box, потом box в anchor
         self.setup_widgets()  # Функция ниже
 
-        self.anchor_layout.add(self.box_layout)  # Box в anchor
-        self.manager.add(self.anchor_layout)  # Всё в manager
-
     def setup_widgets(self):
-        text_area = UITextArea(text="Это UITextArea.\nМного строк!\nДля логов или чата.\nИли блога про своего котика",
-                               width=200,
-                               height=100,
-                               font_size=14)
-        self.anchor_layout.add(text_area)
-
-        label = UILabel(text="Привет, Мир! Это UILabel",
+        # UI - координаты ручками
+        label = UILabel(y=SCREEN_HEIGHT - 50,
+                        text="ll:",
                         font_size=20,
-                        text_color=arcade.color.WHITE,
-                        width=300,
-                        align="center")
-        self.anchor_layout.add(label)
+                        text_color=arcade.color.BLACK,
+                        width=300,)
+        self.manager.add(label)
+
+        input_text = UIInputText(y=SCREEN_HEIGHT - 100,
+                                 width=200,
+                                 height=30,
+                                 text=str(self.ll[0]),
+                                 text_color=arcade.color.BLACK)
+        input_text.on_change = lambda text: self.ll_change(text, 0)
+        self.manager.add(input_text)
+        self.gui_elements.append(input_text)
+
+        input_text = UIInputText(y=SCREEN_HEIGHT - 150,
+                                 width=200,
+                                 height=30,
+                                 text=str(self.ll[1]),
+                                 text_color=arcade.color.BLACK)
+        input_text.on_change = lambda text: self.ll_change(text, 1)
+        self.manager.add(input_text)
+        self.gui_elements.append(input_text)
+
+        label = UILabel(y = SCREEN_HEIGHT - 200,
+                        text="spn:",
+                        font_size=20,
+                        text_color=arcade.color.BLACK,
+                        width=300)
+        self.manager.add(label)
+
+        input_text = UIInputText(y=SCREEN_HEIGHT - 250,
+                                 width=200,
+                                 height=30,
+                                 text=str(self.spn[0]),
+                                 text_color=arcade.color.BLACK)
+        input_text.on_change = lambda text: self.spn_change(text, 0)
+        self.manager.add(input_text)
+        self.gui_elements.append(input_text)
+
+        input_text = UIInputText(y=SCREEN_HEIGHT - 300,
+                                 width=200,
+                                 height=30,
+                                 text=str(self.spn[1]),
+                                 text_color=arcade.color.BLACK)
+        input_text.on_change = lambda text: self.spn_change(text, 1)
+        self.manager.add(input_text)
+        self.gui_elements.append(input_text)
+
+        flat_button = UIFlatButton(text="показать", width=200, height=50, color=arcade.color.BLUE, y = 25)
+        flat_button.on_click = lambda event: self.update_map()
+        self.manager.add(flat_button)
 
     def on_draw(self):
         self.clear()
-        self.manager.draw()  # Рисуй GUI поверх всего
+        arcade.draw_texture_rect(self.Map, arcade.rect.XYWH(SCREEN_WIDTH - MAP_SIZE[0] * MAP_SCALE / 2 - MAP_BORDER,
+                                                            SCREEN_HEIGHT / 2,
+                                                            MAP_SIZE[0] * MAP_SCALE, MAP_SIZE[1] * MAP_SCALE))
+        self.manager.draw()
 
-    def on_mouse_press(self, x, y, button, modifiers):
-        pass  # Для кликов, но manager сам обрабатывает
+    def update_map(self):
+        api_server = "https://static-maps.yandex.ru/v1"
+        # параметр scale, отвечающий за размер, по умолчанию максимально возможное 600x450
+        params = {
+            'll': ",".join(map(str, self.ll)),
+            'spn': ','.join(map(str, self.spn)),
+            'apikey': 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
+        }
+        response = requests.get(api_server, params=params)
+        if not response:
+            raise Exception('ошибка с запросом')
+
+        # этот способ намного быстрее сохранения картинки отдельно
+        # этим способом хоть какая-то плавность при быстром изенении карты (пункт 2 и 3)
+        image = Image.open(BytesIO(response.content))
+        image = image.convert("RGBA")
+        self.Map = arcade.Texture(image)
+
+    def ll_change(self, value, a):
+        try:
+            if value.new_value == '':
+                self.ll[a] = 0
+            else:
+                self.ll[a] = float(value.new_value)
+        except ValueError:
+            self.gui_elements[a].text = value.old_value
+
+    def spn_change(self, value, a):
+        try:
+            if value.new_value == '':
+                self.spn[a] = 0
+            else:
+                self.spn[a] = float(value.new_value)
+        except ValueError:
+            self.gui_elements[a + 2].text = value.old_value
 
 
 if __name__ == "__main__":
